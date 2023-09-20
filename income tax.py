8401c89d92580fb5937535749e4ecdfb6e4f8848
dhruv-DA-coder <145418252+dhruv-DA-coder@users.noreply.github.com>
2023-09-20 19:04:34 +0530

    Add files via upload

---
+++ income tax.py
@@ -0,0 +1,8 @@
+x=float(input("enter a your annual income in rupees:"))
+
+if(x>0):
+    y=x*0.18
+    print("your income tax is ",y)
+
+else:
+    print("your income cannot be less than or equal to zero")
