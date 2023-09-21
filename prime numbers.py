33fcb2ce417c82c46815712637e34c2a48f1628e
dhruv-DA-coder <145418252+dhruv-DA-coder@users.noreply.github.com>
2023-09-21 08:27:10 +0530

    Add files via upload

---
+++ prime numbers.py
@@ -0,0 +1,15 @@
+a=int(input("enter the starting number :"))
+b=int(input("enter the ending number :"))
+
+if(a>0 and b>a):
+    for x in range(a,b+1):
+        for i in range(2,x):
+            if (x%i == 0):
+                print(x,"not a prime number")
+                break
+        else:
+            print (x,"is a prime number")
+else:
+    print(" the starting number should be less than ending number and starting number has to be more than zero")
+
+
