c0660b327931502577d0c7319dbcec437c9f4d61
dhruv-DA-coder <145418252+dhruv-DA-coder@users.noreply.github.com>
2023-09-21 16:46:31 +0530

    check if a number is prime or not.py

--- check if a number is prime or not.py
+++ check if a number is prime or not.py
@@ -1,11 +1,14 @@
 x=int(input("enter the starting number :"))
 
-if(x>0):
+if(x>2):
     for i in range(2,x):
         if (x%i == 0):
             print(x,"not a prime number")
             break
         else:
             print (x,"is a prime number")
-else):
-    print("")
+elif(x==2):
+    print(x,"is a prime")
+
+else:
+    print(x,is not a prime)
